9397938e8eed70b15120934e26726c72b74d6063
Anton Wieslander <Anton.Wieslander@crispthinking.com>
2020-12-30 17:52:40 +0000

    front page styles

--- src/raw_blog/pages/index.clj
+++ src/raw_blog/pages/index.clj
@@ -4,8 +4,8 @@
 
 (defn- blog-preview [key blog]
   [:a {:href (str "/" key)}
-   [:div
-    [:h5 (blog :title)]
+   [:div.post
+    [:h2 (blog :title)]
     [:p (blog :desc)]]])
 
 (defn- blog-previews [blogs]
@@ -13,8 +13,22 @@
     (->> blog-keys
          (map (fn [key] (blog-preview key (blogs key)))))))
 
+(def css "
+a {
+  text-decoration: none;
+  color: white;
+}
+
+.post {
+  padding: 1rem;
+  border-radius: 5px;
+  background: #5f5762;
+}
+")
+
 (def page (blog-previews blg/all))
 
 (defn index-page [ctx]
   (layout {:title "Raw Coding Blog"
+           :css css
            :content page}))
--- src/raw_blog/pages/layout.clj
+++ src/raw_blog/pages/layout.clj
@@ -1,13 +1,29 @@
 (ns raw-blog.pages.layout
   (:require [hiccup.core :refer [html]]))
 
+(def default-css "
+body {
+  background: #1B0F1E;
+}
+
+* {
+  font-family: 'Nunito', sans-serif !important;
+}
+
+div.content {
+  margin: auto;
+  max-width: 800px;
+}
+")
+
 (defn layout [render-ctx]
   (let [{title :title
          css :css
          content :content} render-ctx]
     (html [:head 
            [:title title]
-           [:style css]]
+           [:style (str default-css "\n" css)]]
           [:body
-           [:div]
            [:div.content content]])))
+
+
